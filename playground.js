752343cd30c4b4dde4c9f6a8698817278735cffa
Gary Burgess <gary.burgess@gmail.com>
2012-08-12 15:40:42 +0100

    Adding sections to playground page

--- playground.js
+++ playground.js
@@ -6,65 +6,73 @@ require(["src/reader"], function (reader) {
     
     function trace() {
         var arr = Array.prototype.slice.apply(arguments);
-        var out = document.getElementById("out");
-        out.innerHTML += arr.join(" ") + "\n";
+        var out = document.getElementById(arr[0]);
+        out.innerHTML += arr.slice(1).join(" ") + "\n";
     }
 
     var errored = false;
 
-    function run(name, input) {
+    function run(cat, name, input, runner) {
         if (errored) {
             return;
         }
-        trace(name + ":");
+        console.log(document.getElementById(cat));
+        document.getElementById(cat).parentNode.className = "block populated";
+        trace(cat, name + ":");
         if (Array.isArray(input)) {
             for (var i = 0; i < input.length; i++) {
-                trace(input[i], "=", reader.read(input[i]).join(", "));
+                trace(cat, input[i], "=", runner(input[i]));
             }
         } else {
-            trace(input, "=", reader.read(input).join(", "));
+            trace(cat, input, "=", runner(input));
         }
-        trace();
+        trace(cat);
+    }
+    
+    function read(name, input) {
+        run("reader", name, input, function (x) {
+            return reader.read(x).join(", ");
+        });
     }
 
     window.onload = function () {
 
         var t = new Date().getTime();
 
-        run("symbols", ["a", "lower", "UPPER", "camelCase", "CamelCase", "j0", "hyphen-ated", "under_scored", "dot.ted", ".length", "úņīčőđē"]);
+        read("symbols", ["a", "lower", "UPPER", "camelCase", "CamelCase", "j0", "hyphen-ated", "under_scored", "dot.ted", ".length", "úņīčőđē"]);
         
-        run("ints", "5");
-        run("floats", "5.2");
-        run("hex", "0xFF");
+        read("ints", "5");
+        read("floats", "5.2");
+        read("hex", "0xFF");
         
-        run("strings", ['"word"', '"how long is a piece of..."; string', '"escape \\"this\\""']);
+        read("strings", ['"word"', '"how long is a piece of..."; string', '"escape \\"this\\""']);
         
-        run("comments", ["; sup?", "; an expr:\n(do 1)"]);
+        read("comments", ["; sup?", "; an expr:\n(do 1)"]);
         
-        run("empty paren list", "()");
-        run("empty bracket list", "[]");
-        run("empty brace list", "{}");   
+        read("empty paren list", "()");
+        read("empty bracket list", "[]");
+        read("empty brace list", "{}");   
         
-        run("1-item paren list", "(a)");
-        run("1-item bracket list", "[a]");
-        run("1-item brace list", "{a}");
+        read("1-item paren list", "(a)");
+        read("1-item bracket list", "[a]");
+        read("1-item brace list", "{a}");
         
-        run("2-item paren list", "(a 5)");
-        run("2-item bracket list", "[a 5]");
-        run("2-item brace list", "{a 5}");
+        read("2-item paren list", "(a 5)");
+        read("2-item bracket list", "[a 5]");
+        read("2-item brace list", "{a 5}");
         
-        run("quoting", ["'a", "'5", "'()", "'(a)", "'(a 5)"]);
-        run("quasiquoting", ["`a", "`5", "`()", "`(a)", "`(a 5)"]);
-        run("unquoting", [",a", ",5", ",()", ",(a)", ",(a 5)"]);
-        run("unquote-splicing", [",@a", ",@5", ",@()", ",@(a)", ",@(a 5)"]);
+        read("quoting", ["'a", "'5", "'()", "'(a)", "'(a 5)"]);
+        read("quasiquoting", ["`a", "`5", "`()", "`(a)", "`(a 5)"]);
+        read("unquoting", [",a", ",5", ",()", ",(a)", ",(a 5)"]);
+        read("unquote-splicing", [",@a", ",@5", ",@()", ",@(a)", ",@(a 5)"]);
         
         if (!errored) {
-            trace("It's all good.", new Date().getTime() - t, "ms.");
+            trace("status", "It's all good.", new Date().getTime() - t, "ms.");
         }
     };
 
     window.onerror = function (e) {
-        trace("There was an error:", e);
+        trace("status", "There was an error:", e);
         errored = true;
     };
     
