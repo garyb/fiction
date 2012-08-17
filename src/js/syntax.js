27ab4df924092b7160781e591ce9ea469826e014
Gary Burgess <gary.burgess@gmail.com>
2012-08-17 01:24:46 +0100

    Forgot quasiquote & unquote syntax complications

--- src/js/syntax.js
+++ src/js/syntax.js
@@ -40,16 +40,16 @@ define(["util"], function (util) {
      */
     function checkVar(atoms, expr) {
         if (atoms.length === 0) {
-            error("var: bad syntax - empty declaration", expr);
+            error("var: bad syntax - empty expression", expr);
         }
         if (atoms.length === 1) {
-            error("var: bad syntax - no value specified", expr);
+            error("var: no value specified", expr);
         }
         if (atoms.length > 2) {
-            error("var: bad syntax - expects 2 arguments", expr);
+            error("var: expects 2 arguments", expr);
         }
         if (!isStandardId(atoms[0])) {
-            error("var: bad syntax - invalid identifier", atoms[0]);
+            error("var: invalid identifier", atoms[0]);
         }
     }
 
@@ -63,21 +63,21 @@ define(["util"], function (util) {
      */
     function checkFunction(atoms, expr) {
         if (atoms.length === 0) {
-            error("fn: bad syntax - empty declaration", expr);
+            error("fn: empty expression", expr);
         }
         if (atoms.length === 1) {
-            error("fn: bad syntax - missing arguments or body", expr);
+            error("fn: no body specified", expr);
         }
         var args = atoms[0];
         if (args.type === "list") {
             for (var i = 0, l = args.value.length; i < l; i++) {
                 var arg = args.value[i];
                 if (!isStandardId(arg)) {
-                    error("fn: bad syntax - invalid argument identifier", arg);
+                    error("fn: invalid argument identifier", arg);
                 }
             }
         } else if (!isStandardId(args)) {
-            error("fn: bad syntax - invalid argument identifier", args);
+            error("fn: invalid argument identifier", args);
         }
     }
 
@@ -90,50 +90,114 @@ define(["util"], function (util) {
      */
     function checkAssign(atoms, expr) {
         if (atoms.length === 0) {
-            error("set!: bad syntax - empty assignment", expr);
+            error("set!: empty expression", expr);
         }
         if (atoms.length === 1) {
-            error("set!: bad syntax - missing value", expr);
+            error("set!: no value specified", expr);
         }
         if (atoms.length > 2) {
-            error("set!: bad syntax - too many arguments", expr);
+            error("set!: too many arguments", expr);
         }
         if (!isStandardId(atoms[0]) && !isProp(atoms[0])) {
-            error("set!: bad syntax - invalid assignee", atoms[0]);
+            error("set!: invalid assignee", atoms[0]);
         }
     }
 
     /**
      * Checks whether `atoms` are valid values for an application of the
-     * primitive `if`. `expr` is the full version of the form being checked, for
-     * use in error messages.
+     * primitive `if`. The valid form for `if` is `(if test then else)`
+     * where `test`, `then`, and `else` are valid expressions. The full version 
+     * of the form being checked is provided as `expr` for use in error  
+     * messages.
      */
     function checkIf(atoms, expr) {
         if (atoms.length === 0) {
-            error("if: bad syntax - empty expression", expr);
+            error("if: empty expression", expr);
         }
         if (atoms.length === 1) {
-            error("if: bad syntax - missing then and else clauses", expr);
+            error("if: no then and else clauses", expr);
         }
         if (atoms.length === 2) {
-            error("if: bad syntax - missing else clause", expr);
+            error("if: no else clause", expr);
         }
     }
 
     /**
-     * Creates a function that checks whether `atoms` are valid values for an
-     * application of the primitive `type`, that only expects one argument.
-     * `expr` is the full version of the form being checked, for use in error
-     * messages.
+     * Checks whether `atoms` are valid values for an application of the
+     * primitive `quote`. The valid form for `if` is `(quote datum)`
+     * where `datum` is any s-expression. The full version of the form being 
+     * checked is provided as `expr` for use in error messages.
      */
-    function checkUnaryExpr(type) {
-        return function (atoms, expr) {
-            if (atoms.length === 0) {
-                error(type + ": bad syntax - empty expression", expr);
+    function checkQuote(atoms, expr) {
+        if (atoms.length === 0) {
+            error("quote: empty expression", expr);
+        }
+        if (atoms.length > 1) {
+            error("quote: too many arguments", expr);
+        }
+    }
+    
+    /**
+     * Checks whether `atoms` are valid values for an application of the
+     * primitive `quasiquote`. The valid form for `quasiquote` is 
+     * `(quasiquote datum)` where `datum` is any s-expression. The syntax of any
+     * `unquote` or `unquote-splicing` forms within datum are also checked, 
+     * although the unquoted expressions are not. The  full version of the form 
+     * being checked is provided as `expr` for use in error messages.
+     */
+    function checkQuasiQuote(atoms, expr) {
+        if (atoms.length === 0) {
+            error("quasiquote: empty expression", expr);
+        }
+        if (atoms.length > 1) {
+            error("quasiquote: too many arguments", expr);
+        }
+        checkQuasiQuoteValue(atoms[0]);
+    }
+    
+    /**
+     * Checks `datum` for uses of `unquote` and `unquote-splicing` to ensure 
+     * they are of the correct form - `(unquote expr)` or 
+     * `(unquote-splicing expr)` where `expr` is any valid expression.
+     */
+    function checkQuasiQuoteValue(datum) {
+        if (datum.type === "list" && datum.value.length > 0) {
+            var car = datum.value[0];
+            if (car.type === "symbol") {
+                if (car.value === "unquote") {
+                    if (datum.value.length === 1) {
+                        error("unquote: empty expression", datum);
+                    }
+                    if (datum.value.length > 2) {
+                        error("unquote: too many arguments", datum);
+                    }
+                } else if (car.value === "unquote-splicing") {
+                    error("unquote-splicing: not in list", datum);
+                }
             }
-            if (atoms.length > 1) {
-                error(type + ": bad syntax - too many arguments", expr);
+            for (var i = 0, l = datum.value.length; i < l; i++) {
+                var f = datum.value[i];
+                if (util.checkForm(f, "unquote-splicing")) {
+                    if (f.value.length === 1) {
+                        error("unquote-splicing: empty expression", f);
+                    }
+                    if (f.value.length > 2) {
+                        error("unquote-splicing: too many arguments", f);
+                    }
+                } else {
+                    checkQuasiQuoteValue(datum.value[i]);
+                }
             }
+        }
+    }
+    
+    /**
+     * Rasises an error for primitive form of `type` because it should only 
+     * appear inside a quasiquote.
+     */
+    function quasiQuoteError(type) {
+        return function () {
+            error(type + ": not in quasiquote", arguments[1]);
         };
     }
 
@@ -143,10 +207,10 @@ define(["util"], function (util) {
             "fn": checkFunction,
             "set!": checkAssign,
             "if": checkIf,
-            "quote": checkUnaryExpr("quote"),
-            "quasiquote": checkUnaryExpr("quasiquote"),
-            "unquote": checkUnaryExpr("unquote"),
-            "unquote-splicing": checkUnaryExpr("unquote-splicing")
+            "quote": checkQuote,
+            "quasiquote": checkQuasiQuote,
+            "unquote": quasiQuoteError("unquote"),
+            "unquote-splicing": quasiQuoteError("unquote-splicing")
         }
     };
 });
