be97efbc06529266196d42e53256952b1f0d671b
garyb <gary.burgess@gmail.com>
2012-08-20 16:08:55 +0100

    node & npm stuff
    Compiler now runnable as `./node ./src/js/main.js [input] [output]`
    where [input] is the path to a .fic file to compile and [output] is the
    result file to write

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+/*jshint node: true, bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, regexp: true, undef: true, unused: true, strict: true*/
+(function () {
+
+    "use strict";
+
+    var requirejs = require("requirejs");
+    var fs = require("fs");
+    var path = require("path");
+
+    requirejs.config({ nodeRequire: require });
+
+    requirejs(["reader", "expander", "compiler"], function (reader, expander, compiler) {
+    
+        var args = process.argv;
+        
+        if (args.length !== 4) {
+            console.log("Usage: fiction [input] [output]");
+        } else {
+    
+            var input = process.argv[2];
+            var output = process.argv[3];
+            
+            var handleImport = function (name, k) {
+                k(reader.read(fs.readFileSync(path.resolve(path.dirname(input), name + ".fic"), "utf8")));
+            };
+            
+            var str = fs.readFileSync(input, "utf8");
+            var exprs = reader.read(str);
+            expander.expand(exprs, handleImport, function (result) {
+                var js = compiler.compile(result);
+                fs.writeFileSync(output, js);
+                console.log("Done!");
+            });
+        }
+    });
+
+}());
