5d74239aa84a511e23da5d8ba609f3d78fa22d55
Gary Burgess <gary.burgess@gmail.com>
2012-08-24 00:57:37 +0100

    Added jshint comment to compiled output
    Temporary hack to make it easier to find problems in the output

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,7 @@
             var exprs = reader.read(str);
             expander.expand(exprs, handleImport, function (result) {
                 var js = compiler.compile(result);
+                js = "/*jshint bitwise: true, curly: true, eqeqeq: true, forin: true, indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, regexp: true, undef: true*/\n/*global define*/\n" + js;
                 fs.writeFileSync(output, js);
                 console.log("Done!");
             });
