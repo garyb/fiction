4f1944b7901861b1e03c34ebe23939c6e5ddecd5
Gary Burgess <gary.burgess@gmail.com>
2012-08-26 00:38:18 +0100

    Thing for running tests
    Not even slightly robust

---
+++ src/js/test.js
@@ -0,0 +1,16 @@
+/*jshint node: true, bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, regexp: true, undef: true, unused: true, strict: true*/
+(function () {
+
+    "use strict";
+
+    var requirejs = require("requirejs");
+    var path = require("path");
+    var nodeRequire = require;
+    requirejs.config({
+        nodeRequire: nodeRequire,
+        baseUrl: path.resolve(path.dirname(process.argv[2]))
+    });
+    
+    requirejs([process.argv[2]]);
+    
+}());
